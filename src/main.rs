525d9a55f2d70a934cb43179f3977b1684acba09
Leon Groesch <leon.groesch@jumo.net>
2024-03-29 13:16:21 +0100

    Snake is working; Screen wrapping; Snake extended with space

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,111 @@
-use pixel_engine::frame_data::frame::*;
-use pixel_engine::frame_data::pixel::*;
-use pixel_engine::frame_transfer::transmitter::*;
-use pixel_engine::geometry::ellipse::*;
-use pixel_engine::geometry::rectangle::*;
-use pixel_engine::misc::frame_rate::*;
-use pixel_engine::user_input::keyboard::*;
+use pixel_engine::prelude::*;
 
 use std::time::Duration;
 
 const WIDTH: u8 = 64;
 const HEIGHT: u8 = 64;
-const FRAME_DELTA: Duration = Duration::from_millis(125);
+const FRAME_DELTA: Duration = Duration::from_millis(500);
+
+#[derive(Copy, Clone)]
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right,
+}
+
+impl Direction {
+    fn inverse(&self) -> Self {
+        match self {
+            Direction::Up => Direction::Down,
+            Direction::Down => Direction::Up,
+            Direction::Left => Direction::Right,
+            Direction::Right => Direction::Left,
+        }
+    }
+}
+
+struct Head {
+    rectangle: Rectangle,
+    direction: Direction,
+}
+
+impl Head {
+    fn new() -> Self {
+        let mut rectangle = Rectangle::builder()
+            .position(1, 1)
+            .size(1, 1)
+            .color(Color::from_u32(0xFF))
+            .build();
+        Self {
+            rectangle,
+            direction: Direction::Right,
+        }
+    }
+    fn update(&mut self) {
+        let (mut x, mut y) = move_point((self.rectangle.x, self.rectangle.y), self.direction);
+        (x, y) = move_point((x, y), self.direction);
+        self.rectangle.x = x;
+        self.rectangle.y = y;
+    }
+}
+
+struct Snake {
+    elements: Vec<Rectangle>,
+    append: bool,
+}
+
+impl Snake {
+    fn update(&mut self, head: &Head) {
+        if self.elements.len() > 1 {
+            for i in 0..(self.elements.len() - 1) {
+                let right = self.elements[i + 1].clone();
+                let left = &mut self.elements[i];
+                left.x = right.x;
+                left.y = right.y;
+            }
+        }
+        if let Some(element) = self.elements.last_mut() {
+            element.x = head.rectangle.x;
+            element.y = head.rectangle.y;
+        }
+        if self.append {
+            let mut new_element = head.rectangle.clone();
+            // let (mut x, mut y) = move_point((new_element.x, new_element.y), head.direction.inverse());
+            // (x, y) = move_point((x, y), head.direction.inverse());
+            // new_element.x = x;
+            // new_element.y = y;
+            println!("new_element: ({}|{})", new_element.x, new_element.y);
+            println!("head: ({}|{})", head.rectangle.x, head.rectangle.y);
+            self.elements.push(new_element);
+            self.append = false;
+        }
+    }
+}
+
+fn move_point(point: (u8, u8), direction: Direction) -> (u8, u8) {
+    let mut result = point;
+    match direction {
+        Direction::Up => result.1 = if point.1 == 0 { HEIGHT } else { point.1 - 1 },
+        Direction::Down => {
+            result.1 = (point.1 + 1) % HEIGHT;
+        }
+        Direction::Left => result.0 = if point.0 == 0 { WIDTH } else { point.0 - 1 },
+        Direction::Right => {
+            result.0 = (result.0 + 1) % WIDTH;
+        }
+    }
+    result
+}
 
 fn main() {
     let mut frame = Frame::new(Color::from_u32(0x0), WIDTH, HEIGHT);
 
-    let mut rectangle = Rectangle::builder()
-        .position(1, 1)
-        .size(3, 3)
-        .color(Color::from_u32(0xFF))
-        .build();
-
-    let mut circle = Ellipse::new((16.0, 16.0), 4.0, 4.0);
+    let mut head = Head::new();
+    let mut snake = Snake {
+        elements: Vec::new(),
+        append: false,
+    };
 
     let mut transmitter = Transmitter::new_retry().unwrap();
 
@@ -31,30 +115,33 @@ fn main() {
         keyboard.update();
 
         if frame_rate.next_frame() {
-            for key in keyboard.consume_keys().into_iter() {
+            if let Some(key) = keyboard.consume_keys().last() {
                 match key {
                     Keycode::Left => {
-                        rectangle.x -= 1;
-                        circle.center.0 -= 1.;
+                        head.direction = Direction::Left;
                     }
                     Keycode::Right => {
-                        rectangle.x += 1;
-                        circle.center.0 += 1.;
+                        head.direction = Direction::Right;
                     }
                     Keycode::Up => {
-                        rectangle.y -= 1;
-                        circle.center.1 -= 1.;
+                        head.direction = Direction::Up;
                     }
                     Keycode::Down => {
-                        rectangle.y += 1;
-                        circle.center.1 += 1.;
+                        head.direction = Direction::Down;
+                    }
+                    Keycode::Space => {
+                        snake.append = true;
                     }
                     _ => {}
                 }
             }
             frame.clear();
-            frame.draw(&rectangle);
-            frame.draw(&circle);
+            snake.update(&head);
+            head.update();
+            frame.draw(&(head.rectangle));
+            for element in &snake.elements {
+                frame.draw(element);
+            }
             transmitter.transmit_frame(&frame).unwrap();
         }
     }
